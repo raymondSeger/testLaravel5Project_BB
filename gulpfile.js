a809a9f8de4ca98baaa7f9b80b52038388b716ac
Raymond Seger <raymondseger@live.com>
2016-03-08 17:02:56 +0700

    test using Elixir. Basically just Easier version of Gulp.
    https://laravel.com/docs/5.2/elixir

--- gulpfile.js
+++ gulpfile.js
@@ -12,5 +12,6 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+	// process it and put it into public/assets/css
+    mix.sass('app.scss', 'public/assets/css');
 });
